3b6891cb40bd0ad598d8d885ac00de59265ec3d4
MayaTakenOutOfCode <mayawastaken12@gmail.com>
2025-03-12 14:43:00 +0100

    Task: Add userForm.php

--- php/randNumbers.php
+++ php/randNumbers.php
@@ -2,7 +2,7 @@
   /*
   This piece of code declares an array which is later shuffled and sliced, so it can be used to choose NUM_OF_QUESTIONS amount of random questions from questions.php file to display. 
   */
-  const NUM_OF_QUESTIONS = 30; // const defining how many questions are supossed to be displayed
+  const NUM_OF_QUESTIONS = 10; // const defining how many questions are supossed to be displayed
   $random_numbers = range(0, 69);
   shuffle($random_numbers);
   $random_numbers = array_slice($random_numbers ,0, NUM_OF_QUESTIONS);
